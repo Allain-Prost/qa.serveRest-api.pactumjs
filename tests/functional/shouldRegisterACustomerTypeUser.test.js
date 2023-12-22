1bc6c0bdb937aae73fb5c2d5bac17511bb1b42d5
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-22 11:31:29 -0300

    Retirando a string e adicionando a variável que valida o cadastro com sucesso

--- tests/functional/shouldRegisterACustomerTypeUser.test.js
+++ tests/functional/shouldRegisterACustomerTypeUser.test.js
@@ -6,7 +6,7 @@ describe("Usuário - POST", () => {
 
   it("[TC-01] - Should register a customer type user", async () => {
     let { json } = await postUsuarios({nome: TC01.name, email: TC01.email, password: TC01.password, administrador: TC01.administrador})
-    assert.equal(json.message, 'Cadastro realizado com sucesso', 'A mensagem é diferente do esperado')
+    assert.equal(json.message, TC01.message, 'A mensagem é diferente do esperado')
     expect(json).to.have.property('_id')
     expect(json._id).to.be.a('string')
     expect(json._id).to.have.lengthOf.above(0)
