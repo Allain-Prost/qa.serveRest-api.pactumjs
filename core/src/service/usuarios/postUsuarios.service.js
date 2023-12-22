b8aca86a27c51b3775517879eaab36f6cc34f927
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-21 22:01:34 -0300

    Centralizando a url base do projeto

--- core/src/service/usuarios/postUsuarios.service.js
+++ core/src/service/usuarios/postUsuarios.service.js
@@ -2,7 +2,7 @@ import { specPactumJs } from "../../constants";
 
 export default async function postUsuarios(nome, email, password, administrador) {
   await specPactumJs()
-    .post(`https://serverest.dev/usuarios`)
+    .post(`${process.env.BASE_URL}/usuarios`)
     .withBody({
       nome: nome,
       email: email,
