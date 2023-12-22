42b60b611e0ce6ef43ceefe6e364bec4a1ca23cc
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-21 22:29:00 -0300

    Att caso de teste TC-01

--- tests/functional/shouldRegisterACustomerTypeUser.test.js
+++ tests/functional/shouldRegisterACustomerTypeUser.test.js
@@ -1,15 +1,10 @@
-const { spec } = require("pactum");
+import postUsuarios from "../../core/src/service/usuarios/postUsuarios.service"
+import TC01 from "../../data/functional/usuarios.data"
 
 describe("User", () => {
+
   it("[TC-01] - Should register a customer type user", async () => {
-    await spec()
-      .post("https://serverest.dev/usuarios")
-      .withBody({
-        nome: "Allain Pactum",
-        email: "Allain11@qa.com.br",
-        password: "AllainPactum",
-        administrador: "true",
-      })
-      .expectStatus(201)
+    const responsePostUser = await postUsuarios({nome: TC01.name, email: TC01.email, password: TC01.password, administrador: TC01.administrador})
+    console.log(responsePostUser.json)
   })
 })
