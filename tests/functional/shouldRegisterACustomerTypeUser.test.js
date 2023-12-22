55f3092a6e1704783baf8dcd5691cfd4108d97a9
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-22 11:23:51 -0300

    Atualizando os imports e adicionando as asserções e expects

--- tests/functional/shouldRegisterACustomerTypeUser.test.js
+++ tests/functional/shouldRegisterACustomerTypeUser.test.js
@@ -1,10 +1,14 @@
-import postUsuarios from "../../core/src/service/usuarios/postUsuarios.service"
-import TC01 from "../../data/functional/usuarios.data"
+const { assert, expect } = require("chai")
+const { postUsuarios } = require("../../core/src/service/usuarios/postUsuarios.service.js")
+const { TC01 } = require("../../data/functional/usuarios.data.js")
 
-describe("User", () => {
+describe("Usuário - POST", () => {
 
   it("[TC-01] - Should register a customer type user", async () => {
-    const responsePostUser = await postUsuarios({nome: TC01.name, email: TC01.email, password: TC01.password, administrador: TC01.administrador})
-    console.log(responsePostUser.json)
+    let { json } = await postUsuarios({nome: TC01.name, email: TC01.email, password: TC01.password, administrador: TC01.administrador})
+    assert.equal(json.message, 'Cadastro realizado com sucesso', 'A mensagem é diferente do esperado')
+    expect(json).to.have.property('_id')
+    expect(json._id).to.be.a('string')
+    expect(json._id).to.have.lengthOf.above(0)
   })
 })
