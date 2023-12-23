a84191478ea0e1c60b18b95f054efaad969030b0
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-23 18:57:00 -0300

    Criando o caso de teste TC-09

---
+++ tests/functional/shouldRegisterAUserWithoutAEmail.test.js
@@ -0,0 +1,11 @@
+const { postUsuarios } = require("../../core/src/service/usuarios/postUsuarios.service.js")
+const { TC09 } = require("../../data/functional/usuarios.data.js")
+const { assert } = require("../../constants.js")
+
+describe("Usuário - POST", () => {
+
+  it("[TC-09] - Should register a user without a email", async () => {
+    let { json } = await postUsuarios({nome: TC09.name, email: TC09.email, password: TC09.password, administrador: TC09.administrador, statusCodeResponse: 400})
+    assert.equal(json.email, TC09.message, 'A mensagem é diferente do esperado')
+  })
+})
