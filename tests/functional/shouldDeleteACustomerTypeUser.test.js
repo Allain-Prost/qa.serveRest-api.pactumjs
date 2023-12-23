9795ce2296e8be21b33b4cbaee8d55b307feec8c
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-22 22:27:17 -0300

    Criando o caso de teste TC02

---
+++ tests/functional/shouldDeleteACustomerTypeUser.test.js
@@ -0,0 +1,17 @@
+const { assert } = require("chai")
+const { postUsuarios } = require("../../core/src/service/usuarios/postUsuarios.service.js")
+const { TC02 } = require("../../data/functional/usuarios.data.js")
+const { deleteUsuario } = require("../../core/src/service/usuarios/deleteUsuarios.service.js")
+
+describe("Usuário - POST", () => {
+
+  let userId
+  before('Should register a customer type user', async () => {
+    userId = await postUsuarios({nome: TC02.name, email: TC02.email, password: TC02.password, administrador: TC02.administrador})
+  })
+
+  it("[TC-02] - Should delete a customer type user", async () => {
+    let { json } = await deleteUsuario(userId.json._id)
+    assert.equal(json.message, TC02.message, 'A mensagem é diferente do esperado')
+  })
+})
