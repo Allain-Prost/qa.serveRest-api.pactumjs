aab982e7786ff316e69b931706366ef27a99e8e3
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-12-23 18:57:26 -0300

    Criando o caso de teste TC-08

--- tests/functional/shouldRegisterAUserWithoutAPassword.test.js
+++ tests/functional/shouldRegisterAUserWithoutAPassword.test.js
@@ -5,8 +5,7 @@ const { assert } = require("../../constants.js")
 describe("Usuário - POST", () => {
 
   it("[TC-07] - Should register a user without a password", async () => {
-    let { json } = await postUsuarios({nome: TC07.name, email: TC07.email, password: '', administrador: TC07.administrador, statusCodeResponse: 400})
-    console.log(json)
+    let { json } = await postUsuarios({nome: TC07.name, email: TC07.email, password: TC07.password, administrador: TC07.administrador, statusCodeResponse: 400})
     assert.equal(json.password, TC07.message, 'A mensagem é diferente do esperado')
   })
 })
