707195fbbb6f3e2b0cb769b124ede68ba2b359a1
allainlc <allain.campos@zig.fun>
2023-12-24 14:44:31 -0300

    Criado o objeto que representa os controllers da API

---
+++ data/global/describeName.data.js
@@ -0,0 +1,16 @@
+const describeName = {
+    login: {
+        contract: `Serve Rest API - Contract 'Login'`,
+        acceptance: `Serve Rest API - Acceptance 'Login'`,
+        functional: `Serve Rest API - Functional 'Login'`
+    },
+    usuario: {
+        contract: `Serve Rest API - Contract 'Usuários Controller'`,
+        acceptance: `Serve Rest API - Acceptance 'Usuários Controller'`,
+        functional: `Serve Rest API - Functional 'Usuários Controller'`
+    },
+}
+
+module.exports = {
+    describeName
+}
