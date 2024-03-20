f21ca2a9fa6e68ebc3082d9cf389970d79cb00a1
allainlc <allain.campos@zig.fun>
2024-03-19 22:45:07 -0300

    Add caso de teste TC070

---
+++ tests/functional/produtos/cadastrarProdutos/deveCadastrarProduto.test.js
@@ -0,0 +1,22 @@
+const { assert, expect } = require("../../../../constants")
+const { postLogin } = require("../../../../core/src/service/login/postLogin.service")
+const { postProdutos } = require("../../../../core/src/service/produtos/postProdutos.service")
+const { postUsuarios } = require("../../../../core/src/service/usuarios/postUsuarios.service")
+const { TC070 } = require("../../../../data/produtos/produtos.data")
+
+describe('Produtos - Post', async () => {
+
+	let login, userAdm
+	before('Deve realizar o cadastro de um administrador e realizar login', async () => {
+		userAdm = await postUsuarios({nome: TC070.usuario.name, email: TC070.usuario.email, password: TC070.usuario.password, administrador: TC070.usuario.administrador})
+		login = await postLogin({email: TC070.usuario.email, password: TC070.usuario.password})
+	})
+
+	it('[TC-070] - Deve validar o cadastro de um produto', async () => {
+		let produto = await postProdutos(login.json.authorization, TC070.produto.nome, TC070.produto.preco, TC070.produto.descricao, TC070.produto.quantidade)
+		assert.equal(produto.json.message, TC070.cadastroProduto.message)
+		expect(produto.json).to.have.property('_id')
+    expect(produto.json._id).to.be.a('string')
+    expect(produto.json._id).to.have.lengthOf.above(0)
+	})
+})
