b1aa42ae9278e2250edafb072f0d48033f32db6d
allainlc <allain.campos@zig.fun>
2024-03-20 22:01:00 -0300

    Criando massa de dados para os casos de testes TC071, TC072 e TC073

--- data/produtos/produtos.data.js
+++ data/produtos/produtos.data.js
@@ -18,6 +18,61 @@ const TC070 = {
 	}
 }
 
+const TC071 = {
+	usuario: {
+		name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    administrador: 'true',
+	},
+	produto: {
+		nomeRepetido: faker.commerce.product(),
+		preco: faker.commerce.price({ max: 1000 }),
+		descricao: faker.commerce.productDescription(),
+		quantidade: faker.number.int({ max: 1000 })
+	},
+	cadastroProduto: {
+		message: 'Já existe produto com esse nome'
+	}
+}
+
+const TC072 = {
+	usuario: {
+		name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    administrador: 'true',
+	},
+	produto: {
+		nomeRepetido: faker.commerce.product(),
+		preco: faker.commerce.price({ max: 1000 }),
+		descricao: faker.commerce.productDescription(),
+		quantidade: faker.number.int({ max: 1000 })
+	},
+	tokenVazio: '',
+	cadastroProduto: {
+		message: 'Token de acesso ausente, inválido, expirado ou usuário do token não existe mais'
+	}
+}
+
+const TC073 = {
+	usuario: {
+		name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    administrador: 'false',
+	},
+	produto: {
+		nomeRepetido: faker.commerce.product(),
+		preco: faker.commerce.price({ max: 1000 }),
+		descricao: faker.commerce.productDescription(),
+		quantidade: faker.number.int({ max: 1000 })
+	},
+	cadastroProduto: {
+		message: 'Rota exclusiva para administradores'
+	}
+}
+
 module.exports = {
-	TC070
+	TC070, TC071, TC072, TC073
 }
